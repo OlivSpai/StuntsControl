7940f5d4197981f824f6332c21384458c154e5d8
momotmn <momotmn@hotmail.fr>
2014-11-27 00:40:07 +0100

    re modif affichage prank
    time_elapsed_short sans espace

--- plugins/plugin.stunters.localrecords.php
+++ plugins/plugin.stunters.localrecords.php
@@ -552,12 +552,12 @@ class plugin_stunters_localrecords extends FoxControlPlugin {
 	function time_elapsed_short($secs)
 	{
 		$bit = array(
-			' y'	=> $secs / 31556926 % 12,
-			' w'	=> $secs / 604800 % 52,
-			' d'	=> $secs / 86400 % 7,
-			' h'	=> $secs / 3600 % 24,
-			' m'	=> $secs / 60 % 60,
-			' s'	=> $secs % 60
+			'y'	=> $secs / 31556926 % 12,
+			'w'	=> $secs / 604800 % 52,
+			'd'	=> $secs / 86400 % 7,
+			'h'	=> $secs / 3600 % 24,
+			'm'	=> $secs / 60 % 60,
+			's'	=> $secs % 60
 			);
 		   
 		foreach($bit as $k => $v){
