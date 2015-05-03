7d42a48d058b9854070bbd33e0a36a31ea0f331f
Spaï <olivspai@gmail.com>
2015-05-03 16:55:12 +0200

    removed Mx option, data are only from Stunts master server now

--- include/defines.php
+++ include/defines.php
@@ -4,6 +4,6 @@
 define('nz', "\r\n");
 define('SC_Name', 			'Stunts Control');
 define('SC_ColoredName', 	'$s$o$fffStunts$o $i$s$f51C$f71o$f93n$fa4t$fb5r$fc6o$fd8l$z');
-define('SC_Version', 		'CleanedEdition');
+define('SC_Version', 		'NextVersion');
 
 ?>
